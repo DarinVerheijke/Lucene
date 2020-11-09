a95a8b51ccdb4a3cceae25dd45480e6045169b42
JulesDesmet <jules.desmet@student.uantwerpen.be>
2020-11-09 15:55:29 +0100

    Documented Searcher.java

--- src/com/informationretrieval/lucene/Indexer.java
+++ src/com/informationretrieval/lucene/Indexer.java
@@ -1,38 +1,34 @@
 package com.informationretrieval.lucene;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Paths;
-
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.Version;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
 
 // Based on tutorials on https://www.tutorialspoint.com/lucene/lucene_indexing_process.htm and
 //  http://www.lucenetutorial.com/sample-apps/textfileindexer-java.html
 
 /**
  * A class that offers indexing functionality. The created indices are stored in the directory that is passed to the
- *  object's constructor. Creating indices can be done by either passing a `File` object to `indexFile()` or by passing
- *  a directory path to `createIndex()`.
+ * object's constructor. Creating indices can be done by either passing a `File` object to `indexFile()` or by passing
+ * a directory path to `createIndex()`.
  */
 public class Indexer {
-    private IndexWriter writer;
+    private final IndexWriter writer;
 
 
     /**
-     * The constructor; initialises the `writer` object and opens the indexing directory.
+     * The constructor; initialises the `writer` object.
      *
-     * @param directoryPath The directory used to store the indices.
+     * @param directoryPath The directory used to store the index.
      */
     public Indexer(String directoryPath) throws IOException {
 
@@ -51,13 +47,12 @@ public class Indexer {
 
     /**
      * Creates a `Document` object from the given `File` object. The new object gets the following fields:
-     *  contents, file_name, file_path.
+     * contents, file_name, file_path.
      *
      * @param file The `File` object for which a `Document` object is to be created.
-     *
      * @return The new `Document` object.
      */
-    private Document getDocument(File file) throws IOException{
+    private Document getDocument(File file) throws IOException {
         Document doc = new Document();
 
         TextField cfield = new TextField(Constants.contents, new FileReader(file));
@@ -75,7 +70,7 @@ public class Indexer {
      *
      * @param file The `File` object that is to be added to the index.
      */
-    private void indexFile(File file) throws IOException{
+    private void indexFile(File file) throws IOException {
         System.out.println("Indexing" + file.getCanonicalPath());
         Document doc = getDocument(file);
         writer.addDocument(doc);
@@ -85,14 +80,13 @@ public class Indexer {
      * Adds all files in a directory to the index.
      *
      * @param directoryPath The directory path containing the files that are to be added to the index.
-     *
      * @return The number of files that were added to the index.
      */
-    public int createIndex(String directoryPath) throws IOException{
+    public int createIndex(String directoryPath) throws IOException {
         File[] files = new File(directoryPath).listFiles();
         for (File file : files) {
-            if(!file.isDirectory() && !file.isHidden() && file.exists() && file.canRead()
-            ){
+            if (!file.isDirectory() && !file.isHidden() && file.exists() && file.canRead()
+            ) {
                 indexFile(file);
             }
         }
--- src/com/informationretrieval/lucene/Searcher.java
+++ src/com/informationretrieval/lucene/Searcher.java
@@ -1,8 +1,5 @@
 package com.informationretrieval.lucene;
 
-import java.io.IOException;
-import java.nio.file.Paths;
-
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.CorruptIndexException;
@@ -17,25 +14,52 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
-// Based on tutorials on https://www.tutorialspoint.com/lucene/lucene_indexing_process.htm and http://www.lucenetutorial.com/sample-apps/textfileindexer-java.html
+import java.io.IOException;
+import java.nio.file.Paths;
 
+// Based on tutorials on https://www.tutorialspoint.com/lucene/lucene_indexing_process.htm and
+//  http://www.lucenetutorial.com/sample-apps/textfileindexer-java.html
+
+/**
+ * A class that offers searching functionality. The directory containing the indices of the collection that is being
+ * searched is passed to the object's constructor. Queries can then be passed to the `search()` function, after which
+ * the actual documents from the results can be retrieved using `getDocument()`.
+ */
 public class Searcher {
     IndexSearcher indexSearcher;
     QueryParser queryParser;
     Query query;
-    public Searcher(String directoryPath) throws IOException{
+
+    /**
+     * The constructor; initialises the `indexSearcher` and `queryParser` objects.
+     *
+     * @param directoryPath The directory used to store the index.
+     */
+    public Searcher(String directoryPath) throws IOException {
         Directory dir = FSDirectory.open(Paths.get(directoryPath));
         IndexReader reader = DirectoryReader.open(dir);
         indexSearcher = new IndexSearcher(reader);
         queryParser = new QueryParser(Constants.contents, new StandardAnalyzer());
     }
 
-    public TopDocs search(String searchQuery) throws IOException, ParseException{
+    /**
+     * Searches the collection of documents for documents matching the given query.
+     *
+     * @param searchQuery The query for which documents need to be returned.
+     * @return The documents matching the search query.
+     */
+    public TopDocs search(String searchQuery) throws IOException, ParseException {
         query = queryParser.parse(searchQuery);
         // Can be any number, shows that amount of searches
         return indexSearcher.search(query, Constants.MAX_AMOUNT);
     }
 
+    /**
+     * Returns the `Document` object associated with the score that was returned by `search()`.
+     *
+     * @param scoreDoc The scored document returned by `search()`.
+     * @return The associated `Document` object.
+     */
     public Document getDocument(ScoreDoc scoreDoc) throws CorruptIndexException, IOException {
         return indexSearcher.doc(scoreDoc.doc);
     }
