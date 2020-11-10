9f4a0e6a4d7b1623168609fe34ed3e0fd1a58e3c
JulesDesmet <jules.desmet@student.uantwerpen.be>
2020-11-10 09:42:30 +0100

    Added main() function to Searcher.java

--- src/com/informationretrieval/lucene/Searcher.java
+++ src/com/informationretrieval/lucene/Searcher.java
@@ -14,8 +14,10 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
+import javax.print.Doc;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 // Based on tutorials on https://www.tutorialspoint.com/lucene/lucene_indexing_process.htm and
 //  http://www.lucenetutorial.com/sample-apps/textfileindexer-java.html
@@ -29,6 +31,35 @@ public class Searcher {
     IndexSearcher indexSearcher;
     QueryParser queryParser;
     Query query;
+
+    /**
+     * A main function that performs a query on an index.
+     *
+     * @param args The command line arguments passed to this script.
+     *             The first argument should be the directory in which the index is stored, or "-" if the default
+     *             directory "./Index" should be used.
+     *             The other arguments are the query parameters.
+     */
+    public static void main(String[] args) {
+        try {
+            String index_dir = args[0].equals("-") ? Constants.index_dir : args[0];
+            System.out.println("Searching through index in " + index_dir);
+            Searcher searcher = new Searcher(index_dir);
+
+            String query = String.join("", Arrays.copyOfRange(args, 1, args.length));
+            long start = System.currentTimeMillis();
+            TopDocs hits = searcher.search(query);
+            long end = System.currentTimeMillis();
+
+            System.out.println(hits.totalHits + " documents found, time: " + (end - start) + "ms");
+            for (ScoreDoc score_doc: hits.scoreDocs) {
+                Document document = searcher.getDocument(score_doc);
+                System.out.println("(" + score_doc.score + ") \t" + document.getField("filename").stringValue());
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * The constructor; initialises the `indexSearcher` and `queryParser` objects.
